b3bc2dbe96f7b6a8f084a1f4095c662f69e80439
RB4F505_030\Administrator <kanyarat.bual@bumail.net>
2017-04-22 16:19:29 +0700

    5.กระสุนวิ่ง

---
+++ Assets/Script/Move.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour {
+
+	public float speed;
+
+	// Use this for initialization
+	void Start () {
+		GetComponent<Rigidbody> ().velocity = transform.forward * speed;
+	}
+}
